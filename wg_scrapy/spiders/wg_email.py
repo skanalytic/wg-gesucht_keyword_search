e8ea2b2aefb01f3e417df37adf961d5743d6b217
Samuel King <samuel.d.t.king@hotmail.co.uk>
2018-06-22 23:28:51 +0200

    csv of sent alerts

--- wg_scrapy/spiders/wg_email.py
+++ wg_scrapy/spiders/wg_email.py
@@ -1,10 +1,12 @@
 # load modules
 import pandas as pd
 import smtplib
+import datetime
 
 pd.set_option('display.max_colwidth', -1)
 
 import json
+import os
 
 
 with open('../../config/wg_config.json', 'r') as f:
@@ -15,22 +17,29 @@ from_email = config_info['from_email']
 to_email = config_info['to_email']
 
 #inputs
-keywords = ['cosy']
+keywords = ['helle']
 
 # load files
 df = pd.read_json('wg_results.json')
 
+# check if already sent
+dir_path = 'wg_sent_list.csv'
+if os.path.exists(dir_path) and os.access(dir_path, os.R_OK):
+    df_sent = pd.read_csv(dir_path)
+    df = df[~df['url'].isin(list(df_sent['url']))]
+else:
+    df_sent = pd.DataFrame()
+
 # check if keyword
 text_out_list = []
+df_sent = pd.DataFrame()
 for index, text in enumerate(df['title'] + ' ' + df['description']):
     for word in keywords:
         if word.lower() in text.lower():
-            text_out = str(df.iloc[0]).encode('ascii', 'ignore').decode('ascii')
+            text_out = str(df.iloc[index]).encode('ascii', 'ignore').decode('ascii')
             text_out_list.append(text)
 
-# check if already emailed
-
-
+            # check if already emailed
 
             # if not send email
             server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -45,7 +54,16 @@ for index, text in enumerate(df['title'] + ' ' + df['description']):
             server.sendmail(from_email, to_email, msg)
             server.quit()
 
-            #make record of email
+            df_temp = df.iloc[index]
+            df_temp['alert_status'] = 'alert sent'
+            df_temp['alert_time'] = str(datetime.datetime.now())
+            df_sent = df_sent.append(df_temp)
+
+#make record of email sent
+if os.path.exists(dir_path) and os.access(dir_path, os.R_OK):
+    with open(dir_path, 'a') as f:
+        df_sent.to_csv(f, header=False,index=False)
+else:
+    df_sent.to_csv(dir_path,index=False)
 
 
-            ### MUST REMOVE PASSWORD!!! BEFORE GIT COMMIT !!!!
